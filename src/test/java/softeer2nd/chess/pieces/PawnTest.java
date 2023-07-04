673318ae890cdcc0f50bddcf7cdcd21d449a7d01
RecordOfJun <pritty0731@naver.com>
2023-07-04 11:11:10 +0900

    refactor: PawnTest Pawn 추가 시 두번 째 인자 테스트 추가

--- src/test/java/softeer2nd/chess/pieces/PawnTest.java
+++ src/test/java/softeer2nd/chess/pieces/PawnTest.java
@@ -9,14 +9,14 @@ import static org.assertj.core.api.Assertions.*;
 class PawnTest {
 
     @Test
-    @DisplayName("간단 테스트")
     public void create() {
-        verifyPawn(Pawn.BLACK_COLOR);
-        verifyPawn(Pawn.WHITE_COLOR);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    private void verifyPawn(final String color){
-        Pawn pawn=new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 }
