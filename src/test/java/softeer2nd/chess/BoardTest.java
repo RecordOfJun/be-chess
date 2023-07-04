120de67abec7b258308133736a86387ab1bccadc
RecordOfJun <pritty0731@naver.com>
2023-07-04 17:22:55 +0900

    test : Pawn 구조 변경에 따른 mission2 test코드 삭제

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -18,19 +18,6 @@ class BoardTest {
     }
 
     @Test
-    public void create() throws Exception {
-        verifyBoard(Pawn.WHITE_COLOR, 1, 0);
-        verifyBoard(Pawn.BLACK_COLOR, 2, 1);
-    }
-
-    private void verifyBoard(final String color,final int boardSize,final int index){
-        Pawn pawn = new Pawn(color);
-        board.add(pawn);
-        assertEquals(boardSize, board.size());
-        assertEquals(pawn, board.findPawn(index));
-    }
-
-    @Test
     public void initialize() throws Exception{
         Board board = new Board();
         board.initialize();
