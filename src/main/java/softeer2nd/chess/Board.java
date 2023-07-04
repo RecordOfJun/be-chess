958ca5a44f4ed6bfecc6a133fb6fdfbfd7460797
RecordOfJun <pritty0731@naver.com>
2023-07-04 11:34:17 +0900

    feat: Board initialize, 폰 문자열 출력 함수 추가

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -6,6 +6,19 @@ import java.util.ArrayList;
 
 public class Board {
     private ArrayList<Pawn> pawnList=new ArrayList<>();
+    private ArrayList<Pawn> blackPawnList=new ArrayList<>();
+    private ArrayList<Pawn> whitePawnList=new ArrayList<>();
+    private void initialize(){
+        for(int i=0;i<8;i++){
+            Pawn black=new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
+            blackPawnList.add(black);
+        }
+
+        for(int i=0;i<8;i++){
+            Pawn white=new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION);
+            blackPawnList.add(white);
+        }
+    }
 
     public void add(Pawn pawn){
         pawnList.add(pawn);
@@ -17,5 +30,21 @@ public class Board {
 
     public Pawn findPawn(int index){
         return pawnList.get(index);
+    }
+
+    public String getWhitePawnsResult(){
+        StringBuilder whitePawnsResult=new StringBuilder("");
+        for (Pawn black : blackPawnList) {
+            whitePawnsResult.append(black.getRepresentation());
+        }
+        return whitePawnsResult.toString();
+    }
+
+    public String getBlackPawnResult(){
+        StringBuilder whitePawnsResult=new StringBuilder("");
+        for (Pawn white : whitePawnList) {
+            whitePawnsResult.append(white.getRepresentation());
+        }
+        return whitePawnsResult.toString();
     }
 }
